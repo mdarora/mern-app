a799e3d952fb5219d30e10ccd7f52ed7c2c2259f
MD Arora <mdarora0001@gmail.com>
2021-05-16 21:11:14 +0530

    home route's always active state bug fixed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -12,8 +12,11 @@ const Navbar = () => {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
+                        <li className="nav-item" hidden>
                             <NavLink className="nav-link" to="/">Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" exact={true} to="/">Home</NavLink>
                         </li>
                         <li className="nav-item">
                             <NavLink className="nav-link" to="/about">About</NavLink>
